e0f869a49789ed89b9d4f1140b7746d2423afac6
migueljh <miguelhernandezjp@gmail.com>
2023-04-04 22:26:33 -0300

    Push in order to see new changes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
 import RestaurantsList from "../views/NearbyRestaurants";
 import RestaurantDetails from "../views/RestaurantDetails";
 
